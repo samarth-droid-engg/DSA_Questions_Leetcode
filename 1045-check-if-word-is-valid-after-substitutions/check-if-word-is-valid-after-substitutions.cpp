7bfc6a6e1a85dce05460b7f6c01b30701dd66fdc
Samarth Goel <samarthiitbalumnus@gmail.com>
2026-08-04 19:46:26 +0530

    Time: 558 ms (5.03%) | Memory: 683 MB (5.03%) - LeetSync

--- 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.cpp
+++ 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if (s[0] != 'a')
-            return false;
-        stack<int> st;
-        for (auto ch : s) {
-            if (ch == 'a') {
-                st.push(ch);
-            } else if (ch == 'b') {
-                if (!st.empty() && st.top() == 'a') {
-                    st.push(ch);
-                } else
-                    return false;
-            } else if (ch == 'c') {
-                if (!st.empty() && st.top() == 'b') {
-                    st.pop();
-                    st.pop();
-                } else
-                    return false;
-            }
+        if (s.size() == 0)
+            return true;
+        int fnd = s.find("abc");
+        if (fnd != string::npos) {
+            // found
+            string tleft = s.substr(0, fnd);
+            string tright = s.substr(fnd + 3, s.size());
+            return isValid(tleft + tright);
         }
-        return st.empty();
+        return false;
     }
 };
