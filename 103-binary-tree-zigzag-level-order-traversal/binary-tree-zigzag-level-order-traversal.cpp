61de1df2d38a5ecf4256644135405a747f802846
Samarth Goel <samarthiitbalumnus@gmail.com>
2026-09-11 23:19:16 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (48.84%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if (root == NULL) {
+            return {};
+        }
+        vector<vector<int>> ans;
+        bool flag = 1;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            vector<int> temp;
+            int sz = q.size();
+            if (flag) {
+                for (int i = 0; i < sz; i++) {
+                    TreeNode* curr = q.front();
+                    temp.push_back(curr->val);
+                    q.pop();
+                    if (curr->left != NULL) {
+                        q.push(curr->left);
+                    }
+                    if (curr->right != NULL) {
+                        q.push(curr->right);
+                    }
+                }
+                flag = 0;
+            } else {
+                for (int i = 0; i < sz; i++) {
+                    TreeNode* curr = q.front();
+                    temp.push_back(curr->val);
+                    q.pop();
+                    if (curr->left != NULL) {
+                        q.push(curr->left);
+                    }
+                    if (curr->right != NULL) {
+                        q.push(curr->right);
+                    }
+                }
+                flag = 1;
+                reverse(temp.begin(), temp.end());
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
